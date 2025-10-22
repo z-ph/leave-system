29a3448bbcbf497b02b69a4018904f1d73681e47
zph <zhangpenghui1@mails.gdut.edu.cn>
2025-10-22 21:11:31 +0800

    add baseURL

--- src/api/generated/core/OpenAPI.ts
+++ src/api/generated/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: '',
+    BASE: 'https://csmht.xin/sinin',
     VERSION: '1.0.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
